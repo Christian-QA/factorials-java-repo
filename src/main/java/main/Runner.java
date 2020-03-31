160da8fce838432af797b83ed0863e5c6bc3859b
Chrisctr <credfern96@gmail.com>
2020-03-31 14:56:41 +0100

    Added Runner for completionist's sake

--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -2,4 +2,10 @@ package main;
 
 public class Runner {
 
+	public static void main(String[] args) {
+
+		Factorials executor = new Factorials();
+		System.out.println(executor.factorialStarter(5));
+
+	}
 }
